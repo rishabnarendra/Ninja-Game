330bf73bfb801194abbef69e2b4c1b73c6cf8365
Rishab Narendra <rishabnarendra@mac-air-056.cs.iastate.edu>
2019-05-26 16:58:29 -0500

    Created basic game scene

--- Ninja/Ninja/Ninja/GameViewController.swift
+++ Ninja/Ninja/Ninja/GameViewController.swift
@@ -12,7 +12,21 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    var scene: GameScene!
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Configure the view
+        let skView = view as! SKView
+        skView.isMultipleTouchEnabled = false
+        
+        // Create and configure the scene
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .aspectFill
+        
+        // Present the scene
+        skView.presentScene(scene)
         
     }
 
