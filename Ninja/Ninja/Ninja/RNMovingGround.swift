13adde72401fa8439d807f9a7bebf3c377ba8d17
Rishab Narendra <rishabnarendra@mac-air-056.cs.iastate.edu>
2019-05-26 17:29:02 -0500

    Added moving ground class

---
+++ Ninja/Ninja/Ninja/RNMovingGround.swift
@@ -0,0 +1,17 @@
+//
+//  RNMovingGround.swift
+//  Ninja
+//
+//  Created by Rishab Narendra  on 5/26/19.
+//  Copyright © 2019 Rishab Narendra . All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class RNMovingGround {
+    
+    init(size: CGSize){
+        
+    }
+}
